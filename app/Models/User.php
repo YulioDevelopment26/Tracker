85b486f076bd1ff570035916ca62e6617f6e9dbb
Julio Orozco <yuliodevelopment@gmail.com>
2025-04-06 02:44:33 -0500

    feature: the models are created for each of the tables in the database, the relationships between them are created and which fields can be manipulated and how they will be caste.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,20 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Couchbase\Role;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    /**
+     * @var string
+     */
+    protected $table = 'users';
+
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
@@ -21,6 +29,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'nickname',
+        'hour_value',
+        'work_time',
+        'status',
     ];
 
     /**
@@ -43,6 +55,25 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'nickname' => 'string',
+            'hour_value' => 'integer',
+            'work_time' => 'string',
+            'status' => 'string',
         ];
+    }
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class, 'role_user', 'user_id', 'role_id');
+    }
+
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class, 'project_user', 'user_id', 'project_id');
+    }
+
+    public function tasks(): BelongsTo
+    {
+        return $this->belongsTo(Task::class);
     }
 }
